e7bb44344d3c2c278fbf81b2a1c0f5d497c02ac0
xrhtg <143481267+JiMaobyYaYa@users.noreply.github.com>
2024-09-20 10:46:58 +0800

    Add files via upload

---
+++ dataProcess.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+df = pd.read_csv(r'..\data\HJ2024 06 06 0000 (Float).csv')
